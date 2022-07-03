fcf62f820998ea93b85f15d631dae0be104d0780
Sharikekkk <skvorcovi662@gmail.com>
2022-07-03 21:35:18 +0400

    Добавьте файлы проекта.

---
+++ social network/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    struct Post
+    {
+        int number;
+        string Text;
+        bool score;
+    }
+    class Program
+    {
+        public static List<Post> AllPosts = new List<Post>();
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void WriteAllPosts()
+        {
+            for (int i = 0; i < AllPosts.Count; i++)
+            {
+                Console.WriteLine(AllPosts[i]);
+            }
+        }
+        static void Menu()
+        {
+            WriteAllPosts();
+        }
+        static void Actions()
+        {
+
+        }
+    }
+}
